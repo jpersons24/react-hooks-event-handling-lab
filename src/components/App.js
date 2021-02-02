af169835069c710e0cb087daeba06adab565c889
Jakob Persons <jakobep24@gmail.com>
2021-02-02 07:55:08 -0500

    tests passing

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Keypad from './Keypad';
+import EyesOnMe from './EyesOnMe';
+
+function App() {
+   return (
+      <div>
+         <h1>App Component</h1>
+         <Keypad />
+         <EyesOnMe />
+      </div>
+   )
+}
+
+export default App;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+function Keypad() {
+   function handleOnChange() {
+      console.log('Entering password...')
+   }
+
+   return (
+      <div>
+         <h3>Keypad Component</h3>
+         <input type="password" onChange={handleOnChange}></input>
+      </div>
+   )
+}
+
+export default Keypad;
